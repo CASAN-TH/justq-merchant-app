00ff324c594f1a8afd6dc53fc161740b8bc58640
Natsulelou <nutshapon_lelou@hotmail.com>
2020-05-27 20:04:52 +0700

    moment

--- src/app/queue/queue.module.ts
+++ src/app/queue/queue.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { QueuePageRoutingModule } from './queue-routing.module';
 
 import { QueuePage } from './queue.page';
+import { MomentPipe } from '../pipe/moment.pipe';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { QueuePage } from './queue.page';
     IonicModule,
     QueuePageRoutingModule
   ],
-  declarations: [QueuePage]
+  declarations: [QueuePage, MomentPipe]
 })
 export class QueuePageModule {}
